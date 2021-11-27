6edf82e4e75d381132eb92f3554f84baa7aced6c
Murat <murat.yilmaz@iys.org.tr>
2021-11-27 20:29:31 +0300

    fix: icon

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,16 +3,7 @@
     <div class="q-pa-md row items-start q-gutter-md">
       <q-card class="my-card" flat bordered style="width: 500px">
         <q-card-section horizontal>
-          <q-card-section class="q-pt-xs">
-            <!-- <div class="text-overline">Dr. Murat YILMAZ</div> -->
-            <div class="text-h5 q-mt-sm q-mb-xs">Sonlu Elemanlar Analizi</div>
-            <div class="text-caption text-grey">
-              Teori ve Python Uygulamaları <br />
-              Katı Mekaniğinde Doğrusal ve Statik Çözümlemeler
-            </div>
-          </q-card-section>
-
-          <q-card-section class="col-9 flex flex-center">
+          <q-card-section class="col-12 flex flex-center">
             <q-img class="rounded-borders" src="~assets/img/book1_kapak.jpeg" />
           </q-card-section>
         </q-card-section>
@@ -20,59 +11,147 @@
         <q-separator />
 
         <q-card-actions>
-          <q-btn flat round icon="event" />
-          <!-- <q-btn flat> 7:30PM </q-btn> -->
+          <q-btn flat round icon="shopping_cart" />
           <q-btn flat color="primary"> Satın Al </q-btn>
         </q-card-actions>
       </q-card>
 
-      <!-- <q-card class="my-card">
-        <q-img src="~assets/img/book1_kapak.jpeg" />
+      <q-card class="my-card">
+        <!-- <q-img src="~assets/img/book1_kapak.jpeg" /> -->
 
         <q-card-section>
           <q-btn
             fab
             color="primary"
-            icon="place"
+            icon="laptop"
             class="absolute"
-            style="top: 0; right: 12px; transform: translateY(-50%)"
+            flat
+            style="top: 0; right: 7px; transform: translateY(-10%)"
           />
 
           <div class="row no-wrap items-center">
-            <div class="col text-h6 ellipsis">Cafe Basilico</div>
+            <div class="col text-h6 ellipsis">Kodlar</div>
             <div
               class="
                 col-auto
-                text-grey text-caption
+                text-caption
+                text-bold
                 q-pt-md
+                q-pr-xs
                 row
                 no-wrap
                 items-center
               "
             >
-              <q-icon name="place" />
-              250 ft
+              <!-- <q-icon name="place" /> -->
+              İndir
             </div>
           </div>
 
-          <q-rating v-model="stars" :max="5" size="32px" />
+          <q-rating v-model="stars" :max="5" size="18px" />
         </q-card-section>
 
         <q-card-section class="q-pt-none">
-          <div class="text-subtitle1">$・Italian, Cafe</div>
+          <q-list>
+            <q-item>
+              <q-item-section>
+                <q-item-label>Bölüm 1</q-item-label>
+                <q-item-label caption lines="2"
+                  >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+                  adipiscit elit.</q-item-label
+                >
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>5 min ago</q-item-label>
+                <q-icon name="star" color="yellow" />
+              </q-item-section>
+            </q-item>
+
+            <q-separator spaced inset />
+
+            <q-item>
+              <q-item-section>
+                <q-item-label>Single line item</q-item-label>
+                <q-item-label caption
+                  >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+                  adipiscit elit.</q-item-label
+                >
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>Voted!</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-separator spaced inset />
+
+            <q-item>
+              <q-item-section>
+                <q-item-label>Single line item</q-item-label>
+                <q-item-label caption
+                  >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+                  adipiscit elit.</q-item-label
+                >
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-badge color="teal" label="10k" />
+              </q-item-section>
+            </q-item>
+
+            <q-separator spaced inset />
+
+            <q-item>
+              <q-item-section>
+                <q-item-label>Single line item</q-item-label>
+                <q-item-label caption
+                  >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+                  adipiscit elit.</q-item-label
+                >
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>2 min ago</q-item-label>
+                <div class="text-orange">
+                  <q-icon name="star" />
+                  <q-icon name="star" />
+                  <q-icon name="star" />
+                </div>
+              </q-item-section>
+            </q-item>
+
+            <q-separator spaced inset />
+
+            <q-item>
+              <q-item-section>
+                <q-item-label>Single line item</q-item-label>
+                <q-item-label caption
+                  >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+                  adipiscit elit.</q-item-label
+                >
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>meta</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+
+          <!-- <div class="text-subtitle1">$・Italian, Cafe</div>
           <div class="text-caption text-grey">
             Small plates, salads & sandwiches in an intimate setting.
-          </div>
+          </div> -->
         </q-card-section>
 
         <q-separator />
 
         <q-card-actions>
           <q-btn flat round icon="save" />
-          <q-icon name="github"></q-icon>
-          <q-btn flat color="primary"> Satın Al </q-btn>
+          <!-- <q-icon name="github"></q-icon> -->
+          <q-btn flat color="primary"> Github </q-btn>
         </q-card-actions>
-      </q-card> -->
+      </q-card>
     </div>
   </q-page>
 </template>
@@ -86,7 +165,7 @@ export default defineComponent({
 
   setup() {
     const data = reactive({
-      stars: 4,
+      stars: 5,
     });
 
     const editorInit = () => {};
